26c2854519aa0c25e056941a69919ac0cb3cc5ad
Matheus Pedrosa <matheuspedrosa.contato@gmail.com>
2024-12-03 15:57:53 -0400

    feat: initial commit

---
+++ src/test/java/com/pedrosa/demo/PayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedrosa.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
